62966000c37e6b70f82beaa0922c17c3faf4179b
James Fallon <jamesstephenfallon@gmail.com>
2017-01-11 23:48:42 -0500

    add javascript calculator to project

---
+++ calculator.js
@@ -0,0 +1,9 @@
+function multiply(a, b) {return a*b}
+
+function add(a, b) {return a+b}
+
+function subtract(a, b) {return a-b}
+
+function divide(a, b) {return a/b}
+
+function power(a, b) {return a**b}
